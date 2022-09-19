d6c1796837162f8a6251e77e715d2ac96228a84f
bhola <bhandarimeena71@gmail.com>
2022-09-19 11:35:04 +0530

    [month and date true]

---
+++ day5/month.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+read -p "Enter date:-" date
+read -p "Enter month:-" month
+
+if (( ($month  <=6 &date <=20 ) ))
+then 
+    echo $month $date "true";
+elif(( ( $month >=3  & $month < 6) & ($date<31) ))
+then
+     echo $date $month "statement is true";
+else
+     echo "false";
+
+fi
+
---
+++ file3/run.bat
@@ -0,0 +1,1 @@
+start chrome --new-window --incognito "xmhamster.desi"
